7ca8b406a136cfb163a55dbadce0c738f9c2795e
gerald-nyerer <geraldnyerer100@gmail.com>
2025-09-29 20:19:18 +0300

    Configure Cloudinary storage

--- gerald portfolio/certifications/serializers.py
+++ gerald portfolio/certifications/serializers.py
@@ -6,10 +6,8 @@ class CertificationSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Certification
-        fields = ["id", "created", "modified", "name", "issuer",
-            "category", "issue_date", "expiration_date",
-            "credential_id", "credential_url", "image"]
-        
+        fields = '__all__'
+
     def get_image(self, obj):
         if obj.image:
             return obj.image.url 
