d83083a6b0dabee9829af226912726673b7a9cad
gerald-nyerer <geraldnyerer100@gmail.com>
2025-09-29 16:26:17 +0300

    Configure Cloudinary storage

--- gerald portfolio/gerald_portfolio/settings.py
+++ gerald portfolio/gerald_portfolio/settings.py
@@ -50,6 +50,10 @@ INSTALLED_APPS = [
 
     'corsheaders',
     'rest_framework',
+
+    #cloudinary storage 
+    'cloudinary',
+    'cloudinary_storage',
 ]
 
 MIDDLEWARE = [
@@ -160,3 +164,14 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Cloudinary configuration
+CLOUDINARY_STORAGE = {
+    "CLOUD_NAME": os.getenv("CLOUDINARY_CLOUD_NAME"),
+    "API_KEY": os.getenv("CLOUDINARY_API_KEY"),
+    "API_SECRET": os.getenv("CLOUDINARY_API_SECRET"),
+}
+
+
+DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
