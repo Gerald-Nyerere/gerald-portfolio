1a5dcf85132028d45bfa650c929ad92de368d794
gerald-nyerer <geraldnyerer100@gmail.com>
2025-09-29 17:41:31 +0300

    Configure Cloudinary storage

--- gerald portfolio/gerald_portfolio/settings.py
+++ gerald portfolio/gerald_portfolio/settings.py
@@ -152,8 +152,6 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
